74cd6c9338ce54cbf880446b890600145749207d
M1racleHao <crazyhl@163.com>
2019-02-14 18:40:41 +0800

    增加需要的任务，更新readme

---
+++ src/Command/CrawlTask.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haoliang
+ * Date: 2019-02-14
+ * Time: 17:29
+ */
+
+namespace App\Command;
+
+
+use Symfony\Component\Console\Command\Command;
+
+class CrawlTask extends Command
+{
+
+}
