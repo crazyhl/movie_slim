426d51f2d192ac99fc75aac69d39b0a15c2f0acf
M1racleHao <crazyhl@163.com>
2019-02-26 16:20:37 +0800

    增加 cors 中间件，以及相关本地测试配置

--- config/middleware.php
+++ config/middleware.php
@@ -2,3 +2,11 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(new Tuupola\Middleware\CorsMiddleware([
+    "origin" => ["http://192.168.50.95:8080", "http://localhost:8080"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => [],
+    "headers.expose" => [],
+    "credentials" => true,
+    "cache" => 0,
+]));
